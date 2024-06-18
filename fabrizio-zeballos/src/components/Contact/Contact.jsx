98cc7b3467ce7efe30368530faa41f4bd7f6f12d
FabrizioZeballos <zeballosfab@gmail.com>
2024-06-18 20:47:42 -0300

    Create contact form

--- fabrizio-zeballos/src/components/Contact/Contact.jsx
+++ fabrizio-zeballos/src/components/Contact/Contact.jsx
@@ -1,5 +1,49 @@
 import styles from "./Contact.module.css";
 
+import { useForm, ValidationError } from "@formspree/react";
+
 export const Contact = () => {
-  return <div className={styles.contact}>Contact</div>;
+  const [state, handleSubmit] = useForm("xgegggey");
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
+  return (
+    <div className={styles["form-container"]}>
+      <h2 className={styles.title}>
+        Let&apos;s <span>connect</span>.
+      </h2>
+      <div>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          {/*  <label htmlFor="email">Email Address</label> */}
+
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className={styles["email-input"]}
+            placeholder="Email"
+          />
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+          <textarea
+            id="message"
+            name="message"
+            className={styles["message-input"]}
+            placeholder="Message"
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+          <button
+            type="submit"
+            disabled={state.submitting}
+            className={styles["submit-btn"]}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
